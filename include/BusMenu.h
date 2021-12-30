e7fdf0a81b91ccb20c744a374f2ca448fa7e17aa
shreyashtalele <80088633+shreyashtalele@users.noreply.github.com>
2021-12-30 11:54:57 +0530

    Update BusMenu.h

--- include/BusMenu.h
+++ include/BusMenu.h
@@ -7,12 +7,15 @@ class BusMenu
 {
     public:
         BusMenu();
+        int CreateTestWindow(HINSTANCE hInst);
         virtual ~BusMenu();
 
     protected:
 
     private:
         //TCHAR szClassName[] = _T("Bus Reservation System");
+        HWND hWindow;
+        LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam);
         LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 
 };
