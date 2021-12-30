ebc78235d1f297a80d955997bc05194a2a190a45
Rushi <hrushikeshborakhede1089@gmail.com>
2021-12-30 22:12:48 +0530

    Create Connection.h

---
+++ include/Connection.h
@@ -0,0 +1,16 @@
+class Connection
+{
+private:
+    /* data */
+public:
+    Connection(/* args */);
+    ~Connection();
+};
+
+Connection::Connection(/* args */)
+{
+}
+
+Connection::~Connection()
+{
+}
