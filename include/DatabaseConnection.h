b11431ce0a80af0c417fe0e0935c52acd548fa44
Rushi <hrushikeshborakhede1089@gmail.com>
2022-01-02 19:23:23 +0530

    Update DatabaseConnection.h

--- include/DatabaseConnection.h
+++ include/DatabaseConnection.h
@@ -1,17 +1,20 @@
 #ifndef DATABASECONNECTION_H
 #define DATABASECONNECTION_H
 #include <mysql.h>
+#include <windows.h>
 
 class DatabaseConnection
 {
     public:
+        MYSQL* conn;
         DatabaseConnection(char *);
+        DatabaseConnection();
         virtual ~DatabaseConnection();
+        //void setEditText(HWND);
 
     protected:
 
     private:
-        MYSQL* conn;
         MYSQL_ROW row;
         MYSQL_RES *res;
         int qstate;
